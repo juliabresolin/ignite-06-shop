f117c2c1b23912ab3a9d81964b631c2fed74c161
julia <juliabresolin2@gmail.com>
2022-11-02 21:30:00 -0300

    feat(home): create product layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
+
+import shirt1 from '../assets/shirts/1.png'
+import shirt2 from '../assets/shirts/2.png'
+import shirt3 from '../assets/shirts/3.png'
 
 export default function Home() {
   return (
@@ -7,9 +13,25 @@ export default function Home() {
         <title>ignite shop | julia bresolin</title>
       </Head>
 
-      <main>
-        <span>home works</span>
-      </main>
+      <HomeContainer>
+        <Product>
+          <Image src={shirt1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 01</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image src={shirt1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta 01</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   )
 }
