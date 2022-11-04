4e1ff6139b284931e177bdb9d2e55b35b95f89db
julia <juliabresolin2@gmail.com>
2022-11-04 15:11:27 -0300

    perf(product): format code && remove unused imports

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,7 +3,5 @@ import { useRouter } from 'next/router'
 export default function Product() {
   const { query } = useRouter()
 
-  return (
-    <span>product works: { JSON.stringify(query)}</span>
-  )
+  return <span>product works: {JSON.stringify(query)}</span>
 }
