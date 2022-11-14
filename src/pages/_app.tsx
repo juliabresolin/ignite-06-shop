56494a90196a61314e3fcb9cf4229f1c2d1dc464
julia <juliabresolin2@gmail.com>
2022-11-14 18:37:57 -0300

    feat(header): create cart button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import Image from 'next/image'
+import { Handbag } from 'phosphor-react'
 
 import { globalStyles } from '../styles/global'
 import { Container, Header } from '../styles/pages/app'
@@ -13,6 +14,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+
+        <button type="button">
+          <div>1</div>
+          <Handbag size={24} />
+        </button>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -13,4 +13,48 @@ export const Header = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  button: {
+    padding: '0.75rem',
+    backgroundColor: '$gray800',
+    borderRadius: 6,
+    border: 0,
+    display: 'block',
+    cursor: 'pointer',
+    transition: 'opacity .2s',
+    position: 'relative',
+
+    '&:not(:disabled):hover': {
+      opacity: 0.75,
+    },
+
+    '&:disabled': {
+      cursor: 'not-allowed',
+    },
+
+    svg: {
+      color: '$gray600',
+    },
+
+    div: {
+      width: '1.5rem',
+      height: '1.5rem',
+      borderRadius: '50%',
+      backgroundColor: '$green500',
+      color: '$white',
+      border: '3px solid $gray900',
+
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+      position: 'absolute',
+      top: '-25%',
+      right: '-25%',
+    },
+  },
 })
